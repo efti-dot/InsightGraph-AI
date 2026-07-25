5c2e142959ebbcc677aa66830437df5d77de4c7d
efti-dot <eftih31@gmail.com>
2026-07-25 11:45:56 +0600

    add condition for demo

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -6,3 +6,10 @@ with st.form("new_research"):
     project_name = st.text_input("Project Name")
     research_goal = st.text_area("Research Goal",height=120)
     submitted = st.form_submit_button("Analyze")
+
+if submitted:
+    if not research_goal.strip():
+        st.warning("Please enter a research goal!")
+    else:
+        with st.spinner("Please wait some moment..."):
+            st.success("this is the result.")
