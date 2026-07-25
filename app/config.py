f330525ae5f618b06644ad38f052e48c5032eda9
efti-dot <eftih31@gmail.com>
2026-07-26 02:14:16 +0600

    update settings

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
-class Settings():
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
     openai_api_key: str
-    model_name: str = ""
+    model_name: str = "gpt-4o-mini"
 
 settings = Settings()
