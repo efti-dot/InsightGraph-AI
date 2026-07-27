05c4d920607ef6eaf5f41bc4c2fd0d82bdb303f0
efti-dot <eftih31@gmail.com>
2026-07-27 16:57:15 +0600

    update graph nodes

--- app/graph/build_graph.py
+++ app/graph/build_graph.py
@@ -2,15 +2,18 @@ from langgraph.graph import StateGraph, END
 from app.graph.state import ResearchState
 from app.graph.nodes.supervisor import supervisor_node
 from app.graph.nodes.report_writer import report_writer_node
+from app.graph.nodes.pdf_agent import pdf_agent_node
 
 def build_graph():
     graph = StateGraph(ResearchState)
 
     graph.add_node("supervisor", supervisor_node)
+    graph.add_node("pdf_agent", pdf_agent_node)
     graph.add_node("report_writer", report_writer_node)
 
     graph.set_entry_point("supervisor")
-    graph.add_edge("supervisor", "report_writer")
+    graph.add_edge("supervisor", "pdf_agent")
+    graph.add_edge("pdf_agent", "report_writer")
     graph.add_edge("report_writer", END)
 
     return graph.compile()
